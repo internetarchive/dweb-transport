9f3ef27debb5a71b60d132bcb2a1134d4859a285
Mitra Ardron <mitra@mitra.biz>
2018-04-10 14:14:57 +1000

    Strip out proxy stuff from server (client was done earlier)

--- serviceworker/sw_server.js
+++ serviceworker/sw_server.js
@@ -2,47 +2,10 @@
 
 'use strict'
 
-/*IPFS
-const IPFS = require('ipfs')
-*/
-/*PROXY
-const {createProxyServer} = require('ipfs-postmsg-proxy');
-*/
-/*IPFS
-let node
-*/
 self.addEventListener('install', (event) => {
     console.log('service-worker 2018apr10 1102 installing');
     event.waitUntil(self.skipWaiting());
 })
-/*IPFS
-async function ipfsstart(verbose) {
-    //Just start IPFS - promise resolves when its started
-    const self = this;
-    return new Promise((resolve, reject) => {
-        node = new IPFS({
-            config: {
-                Addresses: {
-                    repo: '/tmp/dweb_ipfsv2700',
-                    Addresses: {
-                        Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
-                    }
-                }
-            }
-        });
-        node.on('ready', () => {
-            resolve();
-        });
-        node.on('error', (err) => reject(err));
-    })
-        .then(() => node.version())
-        .then((version) => console.log('IPFS READY', version))
-        .catch((err) => {
-            console.log("Error caught in ipfsstart");
-            throw(err);
-        });
-}
-*/
 
 self.addEventListener('activate', (event) => {
     console.log('service-worker activating');
@@ -74,24 +37,3 @@ async function p_ping(url, text) {
     const headers = {status: 200, statusText: 'OK', headers: {}};
     return new Response(`${text || "Ping response to:"} ${url}`, headers)
 }
-/*
-async function catAndRespond(hash) {
-    // Fetch file, and return in a HTTP response
-    if (!node) await ipfsstart();
-    const data = await node.files.cat(hash);
-    const headers = {status: 200, statusText: 'OK', headers: {}};
-    return new Response(data, headers)
-}
-*/
-
-/*PROXY
-createProxyServer(() => node, {
-    addListener: self.addEventListener.bind(self),
-    removeListener: self.removeEventListener.bind(self),
-    async postMessage(data) {
-        // TODO: post back to the client that sent the message?
-        const clients = await self.clients.matchAll();
-        clients.forEach(client => client.postMessage(data))
-    }
-});
-*/
