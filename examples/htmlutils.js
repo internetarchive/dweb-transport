74025d4a65485359713ddcc73fa4d628ea57b0a1
Mitra Ardron <mitra@mitra.biz>
2018-05-07 20:01:26 +0000

    Local changes on dweb

--- examples/htmlutils.js
+++ examples/htmlutils.js
@@ -1,6 +1,7 @@
 /*
     This file is a set of utility functions used in the manipulation of HTML pages
     There is nothing specific to Dweb at all here, feel free to copy and modify.
+    TODO-REPO find what from here, if anything, used in archive and remove rest
  */
 
 
@@ -230,17 +231,9 @@ function seteditor(content) {
     tinyMCE.activeEditor.setDirty(true);        // Allow saving this restored text
 }
 
-function starteditor(opts={}) {
-    /*
-    Start TinyMCE editor
-    opts = {
-        savecb(content) => void: Callback on saving
-        ... other tiny initialization options to override defaults
-        }
-     */
-    savecb = opts.savecb;
-    delete opts.savecb;
-    tinydefaults = {
+function starteditor() {
+    //TODO maybe add some options that can override fields if needed (e.g. with a Object.assign
+    tinymce.init({
         selector: '#mytextarea',
         menubar: "true",
         plugins: [ "save",
@@ -248,76 +241,6 @@ function starteditor(opts={}) {
             'searchreplace visualblocks code fullscreen',
             'insertdatetime media table contextmenu paste code' ],
         toolbar: 'save | undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-        save_onsavecallback: () => savecb(tinyMCE.get('mytextarea').getContent())  // This function must be provided
-    };
-    tinymce.init(Object.assign(tinydefaults, opts));
-}
-
-
-// This comes from archive/ReactFake.js
-function createElement(tag, attrs, children) {        // Note arguments is set to tag, attrs, child1, child2 etc
-    /* Replaces React's createElement - has a number of application specific special cases
-        <img src=ArchiveFile(...)> replaced by <div><img x-=u>
-
-     */
-
-    /* First we handle cases where we dont actually build the tag requested */
-
-    const kids = Array.prototype.slice.call(arguments).slice(2);
-
-    /* Special cases go here - see examples in archive/ReactFake.js---- */
-    return buildoutElement(document.createElement(tag), tag, attrs, kids);
-}
-function buildoutElement(element, tag, attrs, kids) {
-    /* Build out a created element adding Attributes and Children
-    tag:    Lower case string of element e.g. "img"
-    attrs:  Object {attr: value}
-    kids:   Array of children
-    /* This is called back by loadImg after creating the tag. */
-    for (let name in attrs) {
-        const attrname = (name.toLowerCase() === "classname" ? "class" : name); // support Reacts "classname"
-        if (name === "dangerouslySetInnerHTML") {
-            element.innerHTML = attrs[name]["__html"];
-            delete attrs.dangerouslySetInnerHTML;
-        }
-        /*
-        // Turn relative URLS in IMG and A into absolute urls - ideally these are also caught by special cases
-        if (["img.src", "a.href"].includes(tag + "." + name) && (typeof attrs[name] === "string") && attrs[name].startsWith('/')) {
-            if (!React._config.root) console.error("Need to React.config({root: 'https://xyz.abc'");
-            attrs[name] = React._config.root + attrs[name];  // e.g. /foo => https://bar.com/foo
-        }
-        */
-        /* Special cases go here - see examples in archive/ReactFake.js---- */
-        if (name && attrs.hasOwnProperty(name)) {
-            let value = attrs[name];
-            if (value === true) {
-                element.setAttribute(attrname, name);
-            } else if (typeof value === "object" && !Array.isArray(value) && attrname !== "source") {
-                // Could also test value.__proto__ !== ({}).__proto__ to only allow plain (not subclassed) objects
-                // e.g. style: {{fontSize: "124px"}} but not things like Domains or SmartDicts
-                for (let k in value) {
-                    element[attrname][k] = value[k];
-                }
-            } else if (typeof value === "object" && !Array.isArray(value) && attrname === "source") {
-                element[attrname] = attrs[name]; // Cant use setAttribute to set a non-attribute to an object.
-            } else if (value !== false && value != null) {
-                element.setAttribute(attrname, value.toString());
-            }
-        }
-    }
-    // Add each of the subdomains
-    for (let i = 0; i < kids.length; i++) {
-        const child = kids[i];
-        if (!child) {
-        } else if (Array.isArray(child)) {
-            child.map((c) => element.appendChild(c.nodeType == null ?
-                document.createTextNode(c.toString()) : c))
-        }
-        else {
-            element.appendChild(
-                child.nodeType == null ?
-                    document.createTextNode(child.toString()) : child);
-        }
-    }
-    return element;
+        save_onsavecallback: () => p_updatecontent(tinyMCE.get('mytextarea').getContent())  // This function must be provided
+    });
 }
