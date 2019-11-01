94d3132363983d37f8d1808d8771b7c3518ff52e
Mitra Ardron <mitra@mitra.biz>
2019-10-31 17:25:27 -0700

    Modify gun test

--- gun/test_gun.js
+++ gun/test_gun.js
@@ -5,6 +5,7 @@
 GUN = require('gun');
 options = {peers: [ "https://dweb.me:4246/gun" ]}
 let g = new GUN(options);
-g.get('/arc').get('archive.org').get('metadata').get('AboutBan1935').once(res => console.log("Result of once", res));
+g.get('arc').get('archive.org').get('metadata').get('AboutBan1935').once(res => console.log("Result of once", res));
 console.log("Trying '.on'");
-g.get('/arc').get('archive.org').get('metadata').get('AboutBan1935').on(res => console.log("Result of on", res));
+// Note shouldnt have leading slash as none in gun_https_archive.js
+g.get('arc').get('archive.org').get('metadata').get('AboutBan1935').on(res => console.log("Result of on", res));
